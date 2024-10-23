0802b119585f430791b28da60a96f03a4d1c7335
Kyle Ingham <kyleingy@proton.me>
2024-10-23 15:48:31 -0600

    Added button styling as a mixin so it can be easily reused.  Created form element for details page and added relevant styling.

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from "@angular/common";
 import { ActivatedRoute } from "@angular/router";
 import { HousingService } from "../housing.service";
 import { HousingLocation } from "../housing-location";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-details",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: `./details.component.html`,
   styleUrls: ["./details.component.scss"],
 })
@@ -15,6 +16,11 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl(""),
+    lastName: new FormControl(""),
+    email: new FormControl(""),
+  });
 
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params["id"]);
