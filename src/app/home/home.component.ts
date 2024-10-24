d30487e1334474ce6759ae15fdb2294755ba72ab
Kyle Ingham <kyleingy@proton.me>
2024-10-23 18:43:21 -0600

    Added search filter functionality to search by city, state or name of housing location. Included 'clear filter' button to reset filter.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,21 +3,48 @@ import { CommonModule } from "@angular/common";
 import { HousingLocationComponent } from "../housing-location/housing-location.component";
 import { HousingLocation } from "../housing-location";
 import { HousingService } from "../housing.service";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-home",
   standalone: true,
-  imports: [CommonModule, HousingLocationComponent],
+  imports: [CommonModule, HousingLocationComponent, ReactiveFormsModule],
   templateUrl: `./home.component.html`,
   styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
+  searchFilter = new FormGroup({
+    search: new FormControl(""),
+  });
 
   constructor() {
-    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
-      this.housingLocationList = housingLocationList;
-    })
+    this.housingService
+      .getAllHousingLocations()
+      .then((housingLocationList: HousingLocation[]) => {
+        this.housingLocationList = housingLocationList;
+        this.filteredLocationList = housingLocationList;
+      });
+  }
+
+  filterResults() {
+    const text = this.searchFilter.value.search ?? "";
+    if (!this.searchFilter.value.search)
+      this.filteredLocationList = this.housingLocationList;
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation) =>
+        housingLocation?.city.toLowerCase().includes(text.toLowerCase()) ||
+        housingLocation?.state.toLowerCase().includes(text.toLowerCase()) ||
+        housingLocation?.name.toLowerCase().includes(text.toLowerCase())
+    );
+
+    this.searchFilter.reset();
+  }
+
+  clearResults(text: string) {
+    this.filteredLocationList = this.housingLocationList;
   }
 }
